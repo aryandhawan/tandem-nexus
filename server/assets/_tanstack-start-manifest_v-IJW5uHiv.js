7e8a6886cc4cb598bddabd6af32e920b28ae7bb2
aryandhawan <Aryandhawan2802@gmail.com>
2026-05-07 13:36:16 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-IJW5uHiv.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Aryan/OneDrive/Desktop/python/tandem-nexus/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DCkItn3S.js"] }, "/": { filePath: "C:/Users/Aryan/OneDrive/Desktop/python/tandem-nexus/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-C5Mc8-Nl.js"] } }, clientEntry: "/assets/index-DCkItn3S.js" });
-export {
-  tsrStartManifest
-};
